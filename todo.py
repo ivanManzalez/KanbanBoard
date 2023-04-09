3812427ba219f7f9166ab81e684bf86577e1ab87
ivanManzalez98 <ivanmanciag@gmail.com>
2023-04-09 19:53:56 -0400

    move data from python to HTML

--- todo.py
+++ todo.py
@@ -1,12 +1,21 @@
+import random
+import string
+from my_data import Data
 ####################################
 import cherrypy
 #http://127.0.0.1:8080/
 
+
 class ToDo(object):
-    
+
+# Cherry py takes the name of the method 
+# as url name (index = '/')
+
     @cherrypy.expose
     def index(self):
-        return open('views/todo.html')
+        return open('views/todo.html').read().format(
+            data= Data
+            )
 
     @cherrypy.expose
     def kanban(self):
@@ -16,14 +25,10 @@ class ToDo(object):
     def backlog(self):
       return open('views/todo_backlog.html')
 
-    # @cherrypy.expose
-    # def index(self):
-    #     return "Hello world!"
-
-    # @cherrypy.expose
-    # def generate(self):
-    #     return ''.join(random.sample(string.hexdigits, 8))
-    
+    @cherrypy.expose
+    ##override default argument by using ?key=value in url
+    def generate(self, length=8):
+        return ''.join(random.sample(string.hexdigits, int(length)))
 
 
 if __name__ == '__main__':
